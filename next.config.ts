7bf53dec5ea3fe4baac3925eee69a01ae7c3452e
SubbarajuGitHub <122296930+SubbarajuGitHub@users.noreply.github.com>
2024-11-22 10:59:46 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+module.exports = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
-
-export default nextConfig;
